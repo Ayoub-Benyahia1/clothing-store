f76abf8faee363cf3953cb4bf3a9c653be531453
AYOUB BENYAHIA <142797314+Ayoub-Benyahia1@users.noreply.github.com>
2025-02-19 17:53:43 +0100

    correct (Send values ​​only when user stops dragging)

--- client/src/components/DualRangeSlider.jsx
+++ client/src/components/DualRangeSlider.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
 import { Slider } from "@/components/ui/slider";
 
 const DualRangeSlider = ({ min = 0, max = 1000, step = 10, updateFilters }) => {
-  const [values, setValues] = useState([min, max]);
+  const { control, setValue, watch, handleSubmit } = useForm({
+    defaultValues: {
+      price_min: min,
+      price_max: max,
+    },
+  });
 
-  const handleChange = (newValues) => {
-    if (newValues[0] < newValues[1]) {
-      setValues(newValues);
-      updateFilters({ price_min: newValues[0], price_max: newValues[1] });
-    }
+  const values = [watch("price_min"), watch("price_max")];
+
+  // Send values ​​only when user stops dragging
+  const handleCommit = () => {
+    handleSubmit((data) => {
+      updateFilters({ price_min: data.price_min, price_max: data.price_max });
+    })();
   };
 
   return (
@@ -17,13 +25,25 @@ const DualRangeSlider = ({ min = 0, max = 1000, step = 10, updateFilters }) => {
         <span>{values[0]}</span>
         <span>{values[1]}</span>
       </div>
-      <Slider
-        value={values}
-        onValueChange={handleChange}
-        min={min}
-        max={max}
-        step={step}
-        className="w-full"
+      <Controller
+        name="priceRange"
+        control={control}
+        render={({ field }) => (
+          <Slider
+            value={values}
+            onValueChange={(newValues) => {
+              if (newValues[0] < newValues[1]) {
+                setValue("price_min", newValues[0]);
+                setValue("price_max", newValues[1]);
+              }
+            }}
+            onValueCommit={handleCommit}
+            min={min}
+            max={max}
+            step={step}
+            className="w-full"
+          />
+        )}
       />
     </div>
   );
