8dc43c2621a6907073a4eeed9551dfdf5aa7e6c5
AYOUB BENYAHIA <142797314+Ayoub-Benyahia1@users.noreply.github.com>
2025-02-24 18:01:44 +0000

    fetch (categories, sizes, colors)

--- client/src/components/shop/Filter.jsx
+++ client/src/components/shop/Filter.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AccordionFilter from "./AccordionFilter";
+import { useDispatch, useSelector } from "react-redux";
+import { allCategories } from "@/redux/slices/categorySlice";
+import { allSizes } from "@/redux/slices/sizeSlice";
+import { allColors } from "@/redux/slices/colorSlice";
 
 function Filter({ updateFilters }) {
+  const { loading, categories, error } = useSelector(
+    (state) => state.categories
+  );
+  const { sizes } = useSelector((state) => state.sizes);
+  const { colors } = useSelector((state) => state.colors);
+  const dispatch = useDispatch();
+  const fetchCategories = async () => {
+    await dispatch(allCategories());
+  };
+  const fetchSizes = async () => {
+    await dispatch(allSizes());
+  };
+  const fetchColors = async () => {
+    await dispatch(allColors());
+  };
+  useEffect(() => {
+    fetchCategories();
+    fetchSizes();
+    fetchColors();
+  }, [dispatch]);
   const labels = [
     {
       name: "Gender",
@@ -14,13 +38,29 @@ function Filter({ updateFilters }) {
     },
     {
       name: "Category",
-      data: ["T-shirts", "Hoodies", "Jeans", "Shirts"],
+      data: loading
+        ? "loading..."
+        : categories.length > 0
+        ? categories.map((category) => category.name)
+        : error,
       filterKey: "category",
     },
-    { name: "Size", data: ["xs", "s", "m", "l", "xl"], filterKey: "size" },
+    {
+      name: "Size",
+      data: loading
+        ? "loading..."
+        : sizes.length > 0
+        ? sizes.map((size) => size.size)
+        : error,
+      filterKey: "size",
+    },
     {
       name: "Color",
-      data: ["White", "Black", "Red", "Blue", "Yellow", "Green"],
+      data: loading
+        ? "loading..."
+        : colors.length > 0
+        ? colors.map((color) => color.color)
+        : error,
       filterKey: "color",
     },
   ];
@@ -32,7 +72,7 @@ function Filter({ updateFilters }) {
           key={index}
           name={label?.name}
           data={label?.data}
-          filterKey={label.filterKey}
+          filterKey={label?.filterKey}
           updateFilters={updateFilters}
         />
       ))}
